c5322fbcf34bfcc925acadb9a8235326d4aea64e
SanjithBC <111051325+SanjithBC@users.noreply.github.com>
2023-06-11 18:56:41 -0700

    Add files via upload

--- book.js
+++ book.js
@@ -9,7 +9,7 @@ function setup() {
     canvas = createCavnas(640, 450);
     canvas.center();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status1").innerHTML = "Status: Detecting Objects";
+    document.getElementById("status3").innerHTML = "Status: Detecting Objects";
 }
 
 function modelLoaded() {
@@ -31,7 +31,7 @@ function draw() {
 
     if(status !="") {
         for (i = 0; i < objects.length; i++) {
-            document.getElementById("status1").innerHTML = "Status: Object Detected";
+            document.getElementById("status3").innerHTML = "Status: Object Detected";
             fill("#ff0000");
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
